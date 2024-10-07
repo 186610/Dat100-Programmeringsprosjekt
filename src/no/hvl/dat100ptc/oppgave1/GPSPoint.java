4b80761dfa45228d69748f89a1435c1210456f76
h186607 <186607@stud.hvl.no>
2024-10-07 18:14:00 +0200

    Oppgave 2 (#2)
    * 2a done
    
    * Cleaning is fun
    
    * Oppg 2b done
    
    * 2c done

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -10,73 +10,45 @@ public class GPSPoint {
 	double elevation;
 	
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
-
-		// TODO - konstruktur
-
 		this.time = time;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.elevation = elevation;
-		
 	}
 
-	// TODO - get/set metoder
 	public int getTime() {
-		
 		return time;
-		
 	}
 
-	public void setTime(int time) {
-				
+	public void setTime(int time) {		
 		this.time = time;
-		
 	}
 
 	public double getLatitude() {
-		
-		return latitude;
-		
-		
+		return latitude;	
 	}
 
 	public void setLatitude(double latitude) {
-		
 		this.latitude = latitude;
-		
 	}
 
 	public double getLongitude() {
-		
 		return longitude;
-		
 	}
 
 	public void setLongitude(double longitude) {
-		
-		this.longitude = longitude;
-		
+		this.longitude = longitude;	
 	}
 
 	public double getElevation() {
-		
-		return elevation;
-		
+		return elevation;	
 	}
 
 	public void setElevation(double elevation) {
-		
-		this.elevation = elevation;
-		
+		this.elevation = elevation;	
 	}
 	
 	public String toString() {
-		
-		String str;
-		
-		return(time + " (" + latitude + "," + longitude + ") " + elevation + "\n");
-
-		// TODO
-		
+		return time + " (" + latitude + "," + longitude + ") " + elevation + "\n";
 	}
 }
--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -6,8 +6,12 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-
-		// TODO
+		GPSPoint gps1 = new GPSPoint(4, 2.5, 3.5, 6.0);
+		GPSPoint gps2 = new GPSPoint(6, 5.0, 7.0, 4.0);
+		GPSData data = new GPSData(2);
 		
+		data.insertGPS(gps1);
+		data.insertGPS(gps2);	
+		data.print();
 	}
 }
