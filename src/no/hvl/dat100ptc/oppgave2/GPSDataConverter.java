b969f207e02861ce864bf587cedab898fc552daf
186610 <186610@stud.hvl.no>
2024-10-13 12:57:13 +0200

    Ola Oppgave 2 ferdig

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -9,22 +9,28 @@ public class GPSDataConverter {
 	private static int TIME_STARTINDEX = 11; 
 
 	public static int toSeconds(String timestr) {
+		 int hours = Integer.parseInt(timestr.substring(11, 13));  
+		    int minutes = Integer.parseInt(timestr.substring(14, 16));  
+		    int seconds = Integer.parseInt(timestr.substring(17, 19));  
+
+		    
+		    return hours * 3600 + minutes * 60 + seconds;
 		
-		int secs;
-		int hr, min, sec;
-		
-		// TODO
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
 
-		GPSPoint gpspoint;
+		 
+	    int timeInSeconds = toSeconds(timeStr);
 
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
+	    
+	    double latitude = Double.parseDouble(latitudeStr);
+	    double longitude = Double.parseDouble(longitudeStr);
+	    double elevation = Double.parseDouble(elevationStr);
+
+	    return new GPSPoint(timeInSeconds, latitude, longitude, elevation);
 	}
-	
+		
+		
 }
